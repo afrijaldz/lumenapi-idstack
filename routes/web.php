fc9a2a7e4c0322cf26035336f6c82d7590e66761
ijul <ijuldz@gmail.com>
2021-11-25 21:10:25 +0700

    add route middleware episod 6

--- routes/web.php
+++ routes/web.php
@@ -23,59 +23,10 @@ $router->get('/key', function () {
     return Str::random(32);
 });
 
-$router->get('/get', function () {
-    return 'GET';
-});
-
-$router->post('/post', function () {
-    return 'POST';
-});
-
-$router->put('/put', function () {
-    return 'PUT';
-});
-
-$router->patch('/patch', function () {
-    return 'PATCH';
-});
-
-$router->delete('/delete', function () {
-    return 'DELETE';
-});
-
-$router->options('/options', function () {
-    return 'OPTIONS';
-});
-
-$router->get('/user/{id}', function ($id) {
-    return 'User id = ' . $id;
-});
-
-$router->get('/post/{postId}/comments/{commentId}', function ($postId, $commentId) {
-    return 'Post ID = ' . $postId . ' Comment ID = ' . $commentId;
-});
-
-// optional parameter
-$router->get('/optional[/{param}]', function ($param = null) {
-    return 'Ini optional param = ' . $param;
-});
-
-// route redirect
-$router->get('/profile', function () {
-    return redirect()->route('route.profile');
-});
-
-$router->get('/profile/idstack', ['as' => 'route.profile', function () {
-    return route('route.profile');
+$router->get('/admin/home', ['middleware' => 'age', function () {
+    return 'Old enough';
 }]);
 
-// grouping
-$router->group(['prefix' => 'admin'], function () use ($router) {
-    $router->get('home', function () {
-        return 'Home Admin';
-    });
-
-    $router->get('profile', function () {
-        return 'Profile Admin';
-    });
+$router->get('/fail', function () {
+    return 'Not yet mature';
 });
